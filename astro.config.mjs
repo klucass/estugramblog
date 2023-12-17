985a2129b281d2d9a76d9450d90f916da2cbccb6
Jose Kennedy Lucas dos Santos Freitas <jose.freitas@fbb.org.br>
2023-12-17 10:21:11 -0300

    Atualização do Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
+import mdx from '@astrojs/mdx';
 
-// https://astro.build/config
-import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
